1c2e995ee940468c1ee6eefdd1660dd4b74d037b
SharpChan <chengfeng3539@126.com>
2020-06-22 18:00:49 +0800

    Initial commit

---
+++ src/main/java/com/sugon/iris/sugoniris/SugonIrisApplication.java
@@ -0,0 +1,13 @@
+package com.sugon.iris.sugoniris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugonIrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugonIrisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sugon/iris/sugoniris/SugonIrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugon.iris.sugoniris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugonIrisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
